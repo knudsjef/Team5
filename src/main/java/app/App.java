29e69856a453146461606a5a761441aeb91cf2bc
Matthew Block <mtblock1@gmail.com>
2020-02-26 19:55:03 -0600

    adding documentation to app class

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -5,16 +5,20 @@ import java.io.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * server to act as an in-between for the website and the database
+ */
 public class App {
-
     public static void main(final String[] args) throws IOException, SQLException, ClassNotFoundException {
-        // server setup
+        // create server
         Jooby app = new Jooby();
 
-        // ssl
+        // locate ssl certificates generated with letsencrypt for the nimmo.us domain
         String certPath = "/etc/letsencrypt/live/nimmo.us/fullchain.pem"; // cert
         String keyPath = "/etc/letsencrypt/live/nimmo.us/privkey.pem"; // key
+        // determine if ssl certificates are available in the environment
         if (new File(certPath).exists()) {
+            // ssl certificates found, run only on secure port 443
             System.out.println("running in secure mode, 8080 and 8443");
             app.setServerOptions(new ServerOptions()
                     .setSecurePort(443)
@@ -22,6 +26,7 @@ public class App {
                     .setSsl(SslOptions.x509(certPath, keyPath))
             );
         } else {
+            // no ssl certificates found, run on port meant for local development
             System.out.println("running in local mode, 8080 only");
             app.setServerOptions(new ServerOptions()
                     .setMaxRequestSize(10485760*1000) // 10mB * 1000
@@ -32,15 +37,17 @@ public class App {
         // allow all cors
         app.decorator(new CorsHandler());
 
-        // test page
+        // address to retrieve a list of all elections stored in the database
         app.get("/getElections", ctx -> {
             ctx.setResponseType(MediaType.json);
 
-            // database setup
+            // gather results from the database
             ResultSet results = Database.query("SELECT * from DigiData.election");
+            // return the results as json for easy processing on the frontend
             return Database.getJSONFromResultSet(results,"results");
         });
 
+        // start the server
         app.start();
     }
 }
